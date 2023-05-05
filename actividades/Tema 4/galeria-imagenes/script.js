097708f229cb403fa638f4052977570d4ae53067
fsanyes <96044318+fsanyes@users.noreply.github.com>
2023-05-05 12:06:03 +0200

    galeria-imagenes

---
+++ actividades/Tema 4/galeria-imagenes/script.js
@@ -0,0 +1,12 @@
+imagenGrande = document.getElementById("largeImg");
+listaImagenes = document.getElementById("thumbs");
+
+listaImagenes.addEventListener("click", e =>{
+
+    if (e.target.parentNode.tagName == "A") {
+        e.preventDefault();
+        imagenGrande.src = e.target.parentNode.href;
+    }
+    else return;
+    
+});
